b7a96895662d1e58d827f9eb33744e9fdf9ea7b8
장선우 <jangsunwoo@MacBook-Air.local>
2026-02-28 18:07:27 +0900

    Update model download script

--- download_models.sh
+++ download_models.sh
@@ -4,6 +4,12 @@ echo "Installing git-lfs..."
 git lfs install
 
 echo "Cloning HuggingFace model repo..."
-git clone https://huggingface.co/Jssun/aidocent models
+git clone https://huggingface.co/Jssun/aidocent temp_models
+
+echo "Moving model folders into project..."
+mv temp_models/* .
+
+echo "Cleaning up..."
+rm -rf temp_models
 
 echo "Model download complete."
